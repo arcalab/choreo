0b5fc70a428a1ab7900fc34932b595e9997de654
José Proença <jose@proenca.org>
2021-01-05 15:40:44 +0000

    implementing Luc's rules

--- src/main/scala/choreo/syntax/Choreo2.scala
+++ src/main/scala/choreo/syntax/Choreo2.scala
@@ -62,6 +62,8 @@ object Choreo2 {
   //  1. a->b  >  b->d  >  b->a  (pomset with 6 nodes)
   //  2. (a->c || c->b) >  b->d  >  b->a   (pomset with 8 nodes)
 
+  val exl1: Choreo2 = ((b→a)>(b→a)) + ((b→a)>(a→c))
+
   val ex1: Choreo2 = (b?"x1"|"m1") + (c?"x2"|"m1") > (b?"x3"|"m2") > (c?"x4"|"m2") // not realsb: b,c do not know if they can receive m2.
   val ex2a: Choreo2 = ((a→b)+(a→c)) > (c→d) // not realsb - c!d must wait for the decision of a, but may not know about it. (Also b waits or not.)
   val ex2b: Choreo2 = ((a→b)+(a→c)) > (d→c) // not realsb (b and c)
@@ -604,6 +606,101 @@ object Choreo2 {
     (a2,simple(b))
   }
 
+
+  /////////////////////////
+  /// Luc's experiments ///
+  /////////////////////////
+
+  def compat(c1:Choreo2,c2:Choreo2): Boolean =
+    c1==c2 || distp(c1,c2)
+
+  def dista(c1:Choreo2,c2:Choreo2): Boolean = (c1,c2) match {
+    case (Send(as, bs, m),_) =>
+      val cs = for (a<-as; b<-bs) yield Out(a,b,m)>In(b,a,m)
+      dista(cs.fold[Choreo2](end)(_>_),c2)
+    case (_,Send(as, bs, m)) =>
+      val cs = for (a<-as; b<-bs) yield Out(a,b,m)>In(b,a,m)
+      dista(c2,cs.fold[Choreo2](end)(_>_))
+
+    case (Seq2(c1a, c1b),Seq2(c2a,c2b)) if c1a==c2a => dista(c1b,c2b)
+    case (Seq2(c1a,_),Seq2(c2a,_)) => dista(c1a,c2a)
+    case (Seq2(c1a,_),_) => dista(c1a,c2)
+    case (_,Seq2(c2a,_)) => dista(c1,c2a)
+    case (Par2(_,_),_) => error("[dista] parallel not handled")
+    case (_,Par2(_,_)) => error("[dista] parallel not handled")
+    case (Choice2(c1a, c1b),_) => dista(c1a,c2) || dista(c1b,c2)
+    case (_,Choice2(c2a, c2b)) => dista(c1,c2a) || dista(c1,c2b)
+    case (Loop2(_),_) => error("[dista] loops nos handled")
+    case (_,Loop2(_)) => error("[dista] loops nos handled")
+    case (Out(a,_,_),Out(b,_,_)) => a==b
+    case (Out(_,_,_),_) => false
+    case (_,Out(_,_,_)) => false
+    case (In(_,_,_),_) => false
+    case (_,In(_,_,_)) => false
+    case (End,End) => false
+    case (End,_) => true // ?
+    case (_,End) => true // ?
+    case _ => error(s"unexpected case: $c1 ~ $c2")
+  }
+
+  def distp(c1:Choreo2, c2:Choreo2): Boolean = (c1,c2) match {
+    case (Send(as, bs, m),_) =>
+      val cs = for (a<-as; b<-bs) yield Out(a,b,m)>In(b,a,m)
+      distp(cs.fold[Choreo2](end)(_>_),c2)
+    case (_,Send(as, bs, m)) =>
+      val cs = for (a<-as; b<-bs) yield Out(a,b,m)>In(b,a,m)
+      distp(c2,cs.fold[Choreo2](end)(_>_))
+
+    case (Seq2(c1a, c1b),Seq2(c2a,c2b)) if c1a==c2a => distp(c1b,c2b)
+    case (Seq2(c1a,_),Seq2(c2a,_)) => distp(c1a,c2a)
+    case (Seq2(c1a,_),_) => distp(c1a,c2)
+    case (_,Seq2(c2a,_)) => distp(c1,c2a)
+    case (Par2(_,_),_) => error("[dista] parallel not handled")
+    case (_,Par2(_,_)) => error("[dista] parallel not handled")
+    case (Choice2(c1a, c1b),_) => distp(c1a,c2) || distp(c1b,c2)
+    case (_,Choice2(c2a, c2b)) => distp(c1,c2a) || distp(c1,c2b)
+    case (Loop2(_),_) => error("[dista] loops nos handled")
+    case (_,Loop2(_)) => error("[dista] loops nos handled")
+    case (End,End) => false
+    case (End,_) => true // ?
+    case (_,End) => true // ?
+    case (In(a1,b1,m1),In(a2,b2,m2)) => (a1==a2) && (b1!=b2) && (m1!=m2)
+    case (In(_,_,_),_) => false
+    case (_,In(_,_,_)) => false
+    case (Out(_,_,_),_) => false
+    case (_,Out(_,_,_)) => false
+    case _ => error(s"unexpected case: $c1 ~ $c2")
+  }
+
+  def compat(ag:Agent2, c1:Choreo2, c2:Choreo2): Boolean =
+    compat(proj(c1,ag),proj(c2,ag))
+  def dista(ag:Agent2, c1:Choreo2, c2:Choreo2): Boolean =
+    dista(proj(c1,ag),proj(c2,ag))
+  def distp(ag:Agent2, c1:Choreo2, c2:Choreo2): Boolean =
+    distp(proj(c1,ag),proj(c2,ag))
+
+
+  def realisableLuc(c:Choreo2): Boolean = c match {
+    case Send(_, _, _) => true
+    case Seq2(c1, c2) =>
+      val r1 = realisableLuc(c1)
+      val r2 = realisableLuc(c2)
+      println(s"[real] '$c1'($r1) ; '$c2'($r2)")
+      r1 && r2
+    case Par2(c1, c2) => error("[real] parallel not handled")
+    case Choice2(c1, c2) =>
+      val r1 = realisableLuc(c1)
+      val r2 = realisableLuc(c2)
+      val ags = agents(c1)++agents(c2)
+      val r3 = ags.forall(a => compat(a,c1,c2))
+      val r4 = ags.exists(b => dista(b,c1,c2) &&
+                               (ags-b).forall(a => compat(a,c1,c2)))
+      println(s"[real] '$c1'($r1) +[$r3,$r4] '$c2'($r2)")
+      r1 && r2 &&  (r3 || r4)
+    case Loop2(c) => error("[real] loops not handled")
+    case End => true
+    case _: Action => true
+  }
 
   /////////////////////////
   //// Extract choices ////
