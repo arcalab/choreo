6b0e70fa0332b4e3f57e989b41ff95fa757c2823
Guillermina Cledou <guillecledou@gmail.com>
2021-02-20 16:47:18 +0000

    tweak semantics of [+] in pomsets

--- src/main/scala/choreo/choreo2/analysis/pomsets/ChoreoPom.scala
+++ src/main/scala/choreo/choreo2/analysis/pomsets/ChoreoPom.scala
@@ -4,6 +4,7 @@ import cats.{Always, Eval, Later, Now}
 import cats.data.State
 import cats.implicits._
 import choreo.choreo2.analysis.Global
+import choreo.choreo2.analysis.Global.{group, nextChoreo}
 import choreo.choreo2.syntax.{Agent, Choreo, Msg}
 import choreo.choreo2.analysis.pomsets.Pomset._
 //import choreo.choreo2.analysis.pomsets.Pomset.LAct
@@ -13,83 +14,83 @@ import choreo.choreo2.view.MermaidPomset
 
 object ChoreoPom:
 
-  type St[A] = State[Int,A]
-  
-  def apply(c:Choreo):Pomset = pomsetOf(c).runA(0).value
+  private var seed:Int = 0
+  private def next():Int = {seed+=1; seed-1}
 
-  private def pomsetOf(c:Choreo):St[Pomset] = c match 
-    case Send(as, bs, m) => 
-      for
-        ps <- as.flatTraverse(a => bs.traverse(b => send(a, b, m)))
-      yield ps.foldRight(identity)(_>>_)
-    case Seq(c1, c2) => 
-      for
-        p1 <- pomsetOf(c1)
-        p2 <- pomsetOf(c2)
-      yield p1 >> p2
-    case Par(c1, c2) => 
-      for
-        p1 <- pomsetOf(c1)
-        p2 <- pomsetOf(c2)
-      yield p1 * p2
-    case Choice(c1, c2) => 
-      for
-        p1 <- pomsetOf(c1)
-        p2 <- pomsetOf(c2)
-      yield p1 + p2
+  def apply(c:Choreo):Pomset = c match
+    case Send(as, bs, m) =>
+      val ps = as.flatMap(a => bs.map(b => send(a, b, m)))
+      val p = ps.foldRight(identity)(_>>_)
+      updateNext(p)
+      p
+    case Seq(c1, c2) =>
+      val p = apply(c1) >> apply(c2)
+      updateNext(p)
+      p
+    case Par(c1, c2) =>
+      val p = apply(c1) * apply(c2)
+      updateNext(p)
+      p
+    case Choice(c1, c2) =>
+      val p = apply(c1) + apply(c2)
+      updateNext(p)
+      p
     case d@DChoice(c1, c2) => // todo: needs work
       //throw new RuntimeException("1-delayed choice not supported yet")
-      val next:List[(Action,Choreo)] = Global.nextChoreo(d)
-      //val p:Pomset =
-      for 
-        nextPoms <- next.traverse(n=> dchoice(n))
-        //_ = nextPoms.map(p=>println(MermaidPomset(p)))
-        m <- State.get
-        //p = mkMChoice(nextPoms,e)
-        _ <- State.set(m+1)
-        e:Int = if nextPoms.flatMap(_.events).nonEmpty then nextPoms.flatMap(_.events).max+1 else m
-      yield Pomset(nextPoms.flatMap(_.events).toSet+e,
-        nextPoms.flatMap(_.labels).toMap+(e->LPoms(nextPoms.toSet)),
-        nextPoms.flatMap(_.order).toSet++nextPoms.flatMap(_.events).map(e1=>Order(e,e1)).toSet+Order(e,e))
-    case Loop(c) => 
-      for 
-        p <- pomsetOf(c)
-      //yield p + identity
-      yield identity + (p >> Pomset(p.events,p.labels,p.order,true))  
-    case End => State.pure(identity)
-    case In(b,a,m):Action => in(b,a,m)
-    case Out(a,b,m): Action => out(a,b,m)
-    case Tau:Action => State.pure(identity) //todo: check
-  
-  
-  //private def mkMChoice(poms:List[Pomset],e:Event):Pomset = {
-  //  var l:Labels = Map()
-  //  for ((p,i) <- poms.zipWithIndex) do 
-  //    l+= (e+i)->LPomp
-  //  ???
-  //}
+      //val nextChor:List[(Action,Choreo)] = Global.nextChoreo(d)
+      //val nextPoms = nextChor.map(n=> dchoice(n))
+      val nc1 = nextChoreo(c1)
+      val nc2 = nextChoreo(c2)
+      val ja = nc1.map(_._1).intersect(nc2.map(_._1))
+      val nj = ja.flatMap(a => group(a,nc1,nc2))
+      val ns = nc1.filterNot(a => ja.contains(a._1)) ++ nc2.filterNot(a=> ja.contains(a._1))
+      var nextPoms:List[Pomset] = List()
+      if nj.nonEmpty then nextPoms = nj.map(dchoice)
+      if ns.nonEmpty then nextPoms ++:= ns.map(dchoiceAlone)//(apply(Choice(c1,c2)))
+      val e = next()
+      var p:Pomset = identity 
+      if (nextPoms.size == 1) then p = nextPoms.head
+        else
+          p = Pomset(nextPoms.flatMap(_.events).toSet+e,
+            nextPoms.flatMap(_.labels).toMap+(e->LPoms(nextPoms.toSet)),
+            nextPoms.flatMap(_.order).toSet++nextPoms.flatMap(_.events).map(e1=>Order(e,e1)).toSet+Order(e,e))
+      updateNext(p)
+      p
+    case Loop(c) =>
+      val pc =  apply(c)
+      val p = identity + (pc >> Pomset(pc.events,pc.labels,pc.order,true))
+      updateNext(p)
+      p
+    case End => identity
+    case act@In(b,a,m):Action =>
+      val e = next()
+      Pomset(Set(e),Map(e->LAct(act)),Set())
+    case act@Out(a,b,m): Action =>
+      val e = next()
+      Pomset(Set(e),Map(e->LAct(act)),Set())
+    case Tau:Action => identity //todo: check
 
-  private def dchoice(next:(Action,Choreo)):St[Pomset] = for
-    pn <- pomsetOf(next._2)
-    //_ = println(s"[dchoice] for $next is ${pn} \n ${MermaidPomset(pn)}")
-    e:Int <- State.get
-    m:Int = if (pn.events).nonEmpty then pn.events.max+1 else e 
-    _ <- State.set(e+2) 
-  yield Pomset(Set(m,m+1)++pn.events,
-    pn.labels++Map(m->LAct(next._1),(m+1)->LPoms(Set(pn))),
-    pn.order++pn.events.map(e1=>Order(m+1,e1)).toSet+Order(m,m+1))
-  
-  private def send(from:Agent,to:Agent,m:Msg):St[Pomset] = for
-    e <- State.get
-    _ <- State.set(e+2)
-  yield Pomset(Set(e,e+1),Map(e->LAct(Out(from,to,m)),(e+1)->LAct(In(to,from,m))),Set(Order(e,e+1))) 
-    
-  private def out(from:Agent,to:Agent,m:Msg):St[Pomset] = for
-    e <- State.get
-    _ <- State.set(e+1)
-  yield Pomset(Set(e),Map(e->LAct(Out(from,to,m))),Set())
 
-  private def in(to:Agent,from:Agent,m:Msg):St[Pomset] = for
-    e <- State.get
-    _ <- State.set(e+1) 
-  yield Pomset(Set(e),Map(e->LAct(In(to,from,m))),Set())
+  private def dchoice(nextChor:(Action,Choreo)):Pomset =
+    val pn =  apply(nextChor._2)
+    val e1 = next()
+    Pomset(Set(e1)++pn.events,  
+      pn.labels++Map(e1->LAct(nextChor._1)),
+      pn.order++pn.events.map(e=>Order(e1,e)).toSet)
+
+  private def dchoiceAlone(nextChor:(Action,Choreo)):Pomset =
+    val pn =  apply(nextChor._2)
+    val e1 = next()
+    val e2 = next()
+    Pomset(Set(e1,e2)++pn.events,
+      pn.labels++Map(e1->LAct(nextChor._1), e2->LPoms(Set(pn))),
+      pn.order++pn.events.map(e=>Order(e2,e)).toSet+Order(e1,e2))
+  
+
+  private def send(from:Agent,to:Agent,m:Msg):Pomset =
+    val e1 = next()
+    val e2 = next()
+    Pomset(Set(e1,e2),Map(e1->LAct(Out(from,to,m)),(e2)->LAct(In(to,from,m))),Set(Order(e1,e2)))
+
+  private def updateNext(p:Pomset):Unit =
+    if p.events.nonEmpty then seed = p.events.max.max(seed)+1
