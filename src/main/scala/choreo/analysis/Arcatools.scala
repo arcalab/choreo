4db521557406dc0ddd9ee92edbe3a615577bfe93
José Proença <jose@proenca.org>
2021-04-22 14:43:56 +0100

    cosmetics to arcatool in choreo

--- src/main/scala/choreo/analysis/Arcatools.scala
+++ src/main/scala/choreo/analysis/Arcatools.scala
@@ -2,7 +2,7 @@ package choreo.analysis
 
 import choreo.analysis.other.SyntAnalysis
 import choreo.pomsets.{Choreo2Pom, GlobalPom, Pomset}
-import choreo.projection.Projection
+import choreo.projection.{ChorDefProj, Projection}
 import choreo.{projection => proj}
 import choreo.sos
 import choreo.sos._
@@ -20,22 +20,20 @@ object Arcatools:
     Network(c,p)
 
   val widgets: List[(Widget,String)] = List(
-    Visualize(ChorMerView,id) ->
-      "Sequence Diagram",
-    Simulate(ChorDefSOS,ChorTxtView,id) ->
-      "Simulate Choreo (Default)",
-    Simulate(ChorBasicSOS,ChorTxtView,id) ->
-      "Simulate Choreo (Basic)",
-    Simulate(ChorManyTausSOS,ChorTxtView,id) ->
-      "Simulate Choreo (ManyTaus)",
-    Simulate(Network.sos(ChorDefSOS),NetwConcView(ChorTxtView),chor2net(proj.ChorDefProj)) ->
-      "Simulate Choreo projetions (default)",
-    Simulate(GlobalPom,PomMerView,chor2pom) ->
-      "Simulate Pomset",
-    Visualize(CollView(PomMerView), c => proj.PomDefProj.allProj(chor2pom(c))) ->
-      "Visualize projections of Pomsets"
+    Visualize(ChorMerView,id) -> "Sequence Diagram",
+    Simulate(ChorDefSOS,ChorTxtView,id) -> "Simulate Choreo (Default)",
+    Simulate(ChorBasicSOS,ChorTxtView,id) -> "Simulate Choreo (Basic)",
+    Simulate(ChorManyTausSOS,ChorTxtView,id) -> "Simulate Choreo (ManyTaus)",
+    simulateNet(ChorDefSOS,ChorTxtView,ChorDefProj,id) -> "Simulate Choreo Network (default)",
+    Simulate(GlobalPom,PomMerView,chor2pom) -> "Simulate Pomset",
+    Visualize(CollView(PomMerView), c => proj.PomDefProj.allProj(chor2pom(c))) -> "Visualize projections of Pomsets"
     //...
   )
+
+//  Simulate(Network.sos(ChorDefSOS),NetwConcView(ChorTxtView),chor2net(proj.ChorDefProj)) ->
+//    "Simulate Choreo projetions (default)",
+//  Simulate(Network.sos(ChorManyTausSOS),NetwConcView(ChorTxtView),chor2net(proj.ChorManyTausProj)) ->
+//    "Simulate Choreo projetions (many-taus)",
 
   val leftWidgets: List[(Widget,String)] = List(
 //    Realisability(proj.ChorDefProj,ChorDefSOS,ChorTxtView,NetwConcView(ChorTxtView),id) ->
@@ -50,6 +48,10 @@ object Arcatools:
     val l:L = Network(p.allProj(g))
     val ls:SOS[Action,L] = Network.sos(sos)
     Bisimulation.pp(Bisimulation.findBisim[G,L](g,l)(using sos,ls))
+
+  def simulateNet[S,T<:ViewTarget](
+             sos:SOS[Action,S],view:View[S,T],proj:Projection[_,S],enc:(Choreo=>S)): Simulate[Action,Network[S]] =
+    Simulate(Network.sos(sos),NetwConcView[S,T](view),(c:Choreo)=>Network(enc(c),proj))
 
   sealed trait Widget
   case class Visualize[S](v:View[S,_],pre:Choreo=>S)                            extends Widget
