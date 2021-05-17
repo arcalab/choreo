f0611ec9447fbe57e1888566803a1bf3b85140ee
Guillermina Cledou <guillecledou@gmail.com>
2021-05-17 16:41:52 +0100

    fix main class

--- src/main/scala/choreo/frontend/Main.scala
+++ src/main/scala/choreo/frontend/Main.scala
@@ -4,4 +4,8 @@ import caos.frontend.Site.initSite
 import choreo.syntax.Choreo
 
 /** Main function called by ScalaJS' compiled javascript when loading. */
-@main def genSite() = initSite[Choreo](ChoreoSOSme)
+object Main {
+  def main(args: Array[String]):Unit = 
+    initSite[Choreo](ChoreoSOSme)
+}
+
